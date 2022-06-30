10c11fc3ba166297b3670975dec7d3735d4fdaf0
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-30 15:36:26 +0900

    [FEAT] MainActivity.kt 주석추가 (#2)

--- app/src/main/java/org/sopt/gittest_android/MainActivity.kt
+++ app/src/main/java/org/sopt/gittest_android/MainActivity.kt
@@ -10,5 +10,11 @@ class MainActivity : AppCompatActivity() {
 
         // TODO : 여기는 나중 ㅔ 어떤 함수가 추가되면 수정해야함!
         // 메인 작업 끝! (feature/MainActivity 브랜치에서 작업함)
+        // feat/main
+        // feat/setting
+
+        // feat/#21
+        // 1. 이슈를 판다 -> # 가 생성됨
+        // 2. feat/#21 브랜치를 만든다
     }
 }
