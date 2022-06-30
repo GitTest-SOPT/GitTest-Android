a3f45100413b6c47e30a1c9693fa9d07f3624c82
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-06-30 15:05:58 +0900

    [INIT] Add todo

--- app/src/main/java/org/sopt/gittest_android/MainActivity.kt
+++ app/src/main/java/org/sopt/gittest_android/MainActivity.kt
@@ -1,11 +1,13 @@
 package org.sopt.gittest_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO : 여기는 나중 ㅔ 어떤 함수가 추가되면 수정해야함!
     }
 }
